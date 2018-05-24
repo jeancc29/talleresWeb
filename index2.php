7117a37f1781251062eb29e4a0bbe41f68399cc5
Jean Contreras <jeancon29@gmail.com>
2018-05-24 11:38:10 -0400

    hola

--- index2.php
+++ index2.php
@@ -32,7 +32,7 @@ $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29",
   $cmd = $conn->prepare("select * from terceros"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
   $cmd->execute();
   $r  =  $cmd->fetchAll();
-  //echo json_encode($r);
+  echo json_encode($r);
 
 }
 
