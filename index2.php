4dd87b4a99467e31b362233aa44e5ab00af0942c
Jean Contreras <jeancon29@gmail.com>
2018-05-24 18:11:43 -0400

    hola

--- index2.php
+++ index2.php
@@ -20,31 +20,31 @@ $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29",
   $cmd = $conn->prepare("select * from terceros"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
   $cmd->execute();
   $r  =  $cmd->fetchAll();
-  echo json_encode($r);
+  //echo json_encode($r);
 
 
 
 
-// if(!empty($data) && $data->action == "datos")
-// {
-//    // echo $data->data;
-// //   $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
-// //   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-// //   $cmd = $conn->prepare("exec sp_clientes_obtener_por_identificacion_nombre  :datos");
-// //   $cmd->execute(array(':datos'=>$data->datos));
-// //   $r  =  $cmd->fetchAll();
-// //   echo json_encode($r);
-
-
-// $serverName = "servidor3.database.windows.net";
-// $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29", "Jean06091929");
+if(!empty($data) && $data->body->action == "datos")
+{
+   // echo $data->data;
+//   $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
 //   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-//   $cmd = $conn->prepare("select * from terceros"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
-//   $cmd->execute();
+//   $cmd = $conn->prepare("exec sp_clientes_obtener_por_identificacion_nombre  :datos");
+//   $cmd->execute(array(':datos'=>$data->datos));
 //   $r  =  $cmd->fetchAll();
 //   echo json_encode($r);
 
-// }
+$serverName = "servidor3.database.windows.net";
+$conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29", "Jean06091929");
+  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  $cmd = $conn->prepare("select * from terceros"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
+  $cmd->execute();
+  $r  =  $cmd->fetchAll();
+  echo json_encode($r);
+
+
+}
 
 
 ?>
