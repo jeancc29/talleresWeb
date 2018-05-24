d6f57fb5c3b9953e0d59ee89cab86bc722a08238
Jean Contreras <jeancon29@gmail.com>
2018-05-24 11:12:58 -0400

    hola

--- index.php
+++ index.php
@@ -20,4 +20,16 @@ $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29",
 
 
 
+// if(!empty($data) && $data->action == "clientes")
+// {
+//    // echo $data->data;
+//   $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
+//   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+//   $cmd = $conn->prepare("exec sp_clientes_obtener_por_identificacion_nombre  :datos");
+//   $cmd->execute(array(':datos'=>$data->datos));
+//   $r  =  $cmd->fetchAll();
+//   echo json_encode($r);
+// }
+
+
 ?>
