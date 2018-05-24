ff3e23916121e36d025a69cdd9ba00759885b99f
Jean Contreras <jeancon29@gmail.com>
2018-05-24 11:08:18 -0400

    Algo nuevo

--- index.php
+++ index.php
@@ -18,15 +18,6 @@ $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "jean29", "J
 
 /* Connect using Windows Authentication. */
 
-if(!empty($data) && $data->action == "clientes")
-{
-   // echo $data->data;
-  $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
-  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  $cmd = $conn->prepare("exec sp_clientes_obtener_por_identificacion_nombre  :datos");
-  $cmd->execute(array(':datos'=>$data->datos));
-  $r  =  $cmd->fetchAll();
-  echo json_encode($r);
-}
+
 
 ?>
