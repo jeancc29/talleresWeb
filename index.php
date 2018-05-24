3417d72cb0edcb3b46d03bf5a48846037f1a3202
Jean Contreras <jeancon29@gmail.com>
2018-05-24 11:04:42 -0400

    Modificando de nuevo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $data=json_decode(file_get_contents("php://input"));
 
 //$serverName = "paginaweb1.database.windows.net";
 $serverName = "servidor3.database.windows.net";
-$conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
+$conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "jean29", "Jean06091929");
   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
   $cmd = $conn->prepare("select * from terceros"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
   $cmd->execute(array(':datos'=>$data->datos));
