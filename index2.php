612095f42f7995b4c7f8ea6b3cafe227258d2251
Jean Contreras <jeancon29@gmail.com>
2018-05-24 18:23:19 -0400

    hola

--- index2.php
+++ index2.php
@@ -25,7 +25,7 @@ $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29",
 
 
 
-if(!empty($data) && $data->action == "datos")
+if(!empty($data) && $data["action"] == "datos")
 {
    // echo $data->data;
 //   $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
