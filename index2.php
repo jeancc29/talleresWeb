5f2bbfab08d3e763d012349ae95e0e0fd99b3fd6
Jean Contreras <jeancon29@gmail.com>
2018-05-25 15:04:31 -0400

    hola

--- index2.php
+++ index2.php
@@ -7,20 +7,19 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 $data=json_decode(file_get_contents("php://input"));
 
+/*
+  Data desde flutter, aqui lo que hice fue insertar la funcion json_encode
+  dentro de la funcion json_decode porque flutter esta enviando un string
+  por lo tanto la funcion json_decode no lo reconoce, asi que lo primero que
+  hago es convertir el string que recibo desde flutter en un json y luego entonces
+  ya si puedo decodificarlo
+*/
+$d = json_decode( json_encode( file_get_contents("php://input"))); 
+
 //$serverName = "paginaweb1.database.windows.net";
 
 
 /* Connect using Windows Authentication. */
-
-//echo $data->action;
-
-if($_REQUEST['REQUEST_METHOD'] == "POST"){
-  $post = filter_input_array(INPUT_POSRRT, FILTER_SANITIZE_STRING);
-  echo "Post: " . $post;
-}
-else{
-  echo "No: ". json_decode( json_encode( file_get_contents("php://input"))) ;
-}
 
 // $serverName = "servidor3.database.windows.net";
 // $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29", "Jean06091929");
@@ -34,26 +33,26 @@ else{
 
 
 
-// if(!empty($data) && $data->action == "datos")
-// {
-//    // echo $data->data;
-// //   $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
-// //   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-// //   $cmd = $conn->prepare("exec sp_clientes_obtener_por_identificacion_nombre  :datos");
-// //   $cmd->execute(array(':datos'=>$data->datos));
-// //   $r  =  $cmd->fetchAll();
-// //   echo json_encode($r);
-
-// $serverName = "servidor3.database.windows.net";
-// $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29", "Jean06091929");
+if(!empty($data) && $d->action == "datos")
+{
+   // echo $data->data;
+//   $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
 //   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-//   $cmd = $conn->prepare("select * from terceros"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
-//   $cmd->execute();
+//   $cmd = $conn->prepare("exec sp_clientes_obtener_por_identificacion_nombre  :datos");
+//   $cmd->execute(array(':datos'=>$data->datos));
 //   $r  =  $cmd->fetchAll();
 //   echo json_encode($r);
 
+$serverName = "servidor3.database.windows.net";
+$conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29", "Jean06091929");
+  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  $cmd = $conn->prepare("select * from terceros"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
+  $cmd->execute();
+  $r  =  $cmd->fetchAll();
+  echo json_encode($r);
 
-// }
+
+}
 
 
 ?>
