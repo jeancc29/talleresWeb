c43605cf8d81f6c7c1b1f60143db76d3d579a438
Jean Contreras <jeancon29@gmail.com>
2018-05-25 16:15:39 -0400

    hola

--- index2.php
+++ index2.php
@@ -32,7 +32,7 @@ $d =   file_get_contents("php://input");
 //   echo json_encode($data);
 json_decode($d);
 
-echo $d . " - " . json_decode($d) ;
+echo $d . " - " . $data ;
 
 if(!empty($d) && $d->action == "datos")
 {
