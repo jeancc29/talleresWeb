23923fe0f4b5e5ebb4a3aede485879f5b3f29132
Jean Contreras <jeancon29@gmail.com>
2018-05-25 16:32:30 -0400

    hola

--- index2.php
+++ index2.php
@@ -14,7 +14,7 @@ $data=json_decode(file_get_contents("php://input"));
   hago es convertir el string que recibo desde flutter en un json y luego entonces
   ya si puedo decodificarlo
 */
-$d =   file_get_contents("php://input"); 
+$d =json_decode(file_get_contents("php://input")); 
 //$d = json_decode($d);
 
 //$serverName = "paginaweb1.database.windows.net";
