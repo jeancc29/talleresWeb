f43a782b5883dbea6bdcb14a49a74a2abef3769f
Jean Contreras <jeancon29@gmail.com>
2018-05-28 11:39:21 -0400

    Otro

--- index2.php
+++ index2.php
@@ -77,5 +77,26 @@ $conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29",
 
 }
 
+if(!empty($d) && $d["action"] == "proyectosCodCliente")
+{
+   // echo $data->data;
+//   $conn = new PDO( "sqlsrv:server=$serverName ; Database=prestamos2", "sa", "123");
+//   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+//   $cmd = $conn->prepare("exec sp_clientes_obtener_por_identificacion_nombre  :datos");
+//   $cmd->execute(array(':datos'=>$data->datos));
+//   $r  =  $cmd->fetchAll();
+//   echo json_encode($r);
+
+$serverName = "servidor3.database.windows.net";
+$conn = new PDO( "sqlsrv:server=$serverName ; Database=talleresAzure", "jean29", "Jean06091929");
+  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  $cmd = $conn->prepare("select * from vw_proyectos where codCliente = :codCliente"); //exec sp_clientes_obtener_por_identificacion_nombre  :datos
+  $cmd->execute(array(":codCliente" => $d["codCliente"]));
+  $r  =  $cmd->fetchAll();
+  echo json_encode($r);
+
+
+}
+
 
 ?>
