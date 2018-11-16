0ca555a3c214a76dabe4cfbd5d8006aaec5fabd3
jeancc29 <jeancon29@gmail.com>
2018-11-16 07:07:55 -0400

    angular coneexion permisos

--- index_angular.php
+++ index_angular.php
@@ -3,6 +3,12 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+
 $data=json_decode(file_get_contents("php://input"));
 
 //$serverName = "prestamoserver.database.windows.net";
